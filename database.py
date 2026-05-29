b7a07a678c533b1c480cc9d5f2afce12cac642ad
Wagner Medeiros dos Santos <wagner.redes@gmail.com>
2026-05-29 16:44:54 -0300

    Fix database.py indentation

--- database.py
+++ database.py
@@ -6,11 +6,11 @@ engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
- pass
+    pass
 
 def get_db():
- db = SessionLocal()
- try:
- yield db
- finally:
- db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
