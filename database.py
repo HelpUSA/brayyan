91e991edb0acb3a31f7de9dcb9de44a48fb9cbdc
Wagner Medeiros dos Santos <wagner.redes@gmail.com>
2026-06-03 22:04:55 -0300

    Fix Postgres engine connect args

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+﻿from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
-SessionLocal = sessionmaker(bind=engine)
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
-class Base(DeclarativeBase):
-    pass
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
